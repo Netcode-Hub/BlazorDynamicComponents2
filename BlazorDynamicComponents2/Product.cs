924fa0d18429ef9b5db82c77bde7e3e076ae152b
Netcode-Hub <Rastinga1995@hotmail.com>
2023-11-18 03:43:06 +0000

    Add project files.

---
+++ BlazorDynamicComponents2/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorDynamicComponents2
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+    }
+}
---
+++ BlazorDynamicComponents2/ProductState.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorDynamicComponents2
+{
+    public class ProductState
+    {
+        public Action<Dictionary<string, object>>? ActionWithProduct { get; set; }
+    }
+}
